87b1335818426d6dad49030c3d8b5aabcce4fcc6
jciarelli <jciarelli@gmail.com>
2017-02-23 18:45:43 -0500

    Added Sass Gulp tasks watch and compile

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,39 @@
 let gulp = require('gulp');
 let git = require('gulp-git');
 let prompt = require('gulp-prompt');
+let sass = require('gulp-sass');
+let maps = require('gulp-sourcemaps');
 
-gulp.task("hello", function(){
-	console.log("hi, this works!");
+// SASS TASKS //
+
+//compile scss file
+gulp.task("compileSass", function(){
+	return gulp.src("scss/style.scss")
+	.pipe(maps.init())
+	.pipe(sass())
+	.pipe(maps.write('./'))
+	.pipe(gulp.dest('css'));
+	});
+// watches scss file
+gulp.task("watchSass", function() {
+	gulp.watch('scss/*.scss', ['compileSass']);
 	});
 
+
+// GITHUB TASKS //
+
+// git init
 gulp.task("init", function(){
 	git.init(function(err) {
 		if (err) throw err;
 		})
 	});
-
+// git add - adds all files
 gulp.task("add", function() {
 	return gulp.src('./*')
 	.pipe(git.add());
 	});
-
+// git commit - commits added files and ignores the .gitignore files, prompts for a commit message
 gulp.task("commit", function(){
 	 gulp.src('package.json')
 	.pipe(prompt.prompt({
@@ -30,13 +47,9 @@ gulp.task("commit", function(){
 	.pipe(git.commit(res.commit));
 	}));
 	});
-
+// git push - to origin master
 gulp.task("push", function() {
 	git.push('origin', 'master', function(err) {
 		if (err) throw err;
 		});
-	});
-
-gulp.task("default", ["hello"], function(){
-	console.log("this is the default");
 	});
